db3b9cc8de33a47eb16bb96fce881b65ba5c32db
Chanagun Pattana <gunkoalaz@gmail.com>
2022-05-08 15:49:14 +0700

    build fix decimal

---
+++ build/precache-manifest.d89355bfafe6d3c86facc824b9e57b34.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "49b43b90444290aa0a00",
+    "url": "/static/css/main.b9dbaeb5.chunk.css"
+  },
+  {
+    "revision": "49b43b90444290aa0a00",
+    "url": "/static/js/main.807cddd5.chunk.js"
+  },
+  {
+    "revision": "910cb7f315bc7f73e4d2",
+    "url": "/static/js/runtime~main.c95f34f5.js"
+  },
+  {
+    "revision": "54a11d4bdeac95a22405",
+    "url": "/static/css/2.0c5e5c3d.chunk.css"
+  },
+  {
+    "revision": "54a11d4bdeac95a22405",
+    "url": "/static/js/2.b2aa76e3.chunk.js"
+  },
+  {
+    "revision": "7c6e50cfd75454170868",
+    "url": "/static/js/3.e7511fc2.chunk.js"
+  },
+  {
+    "revision": "5aaa5af11501d688c8f0f677379bdb64",
+    "url": "/static/media/armlogo.5aaa5af1.png"
+  },
+  {
+    "revision": "fe9bc614df69ea3b7b65f73b0952d205",
+    "url": "/static/media/btclogo.fe9bc614.png"
+  },
+  {
+    "revision": "a440d4b512f4d2b9b63d3ab8818fc9e3",
+    "url": "/static/media/tetherlogo.a440d4b5.png"
+  },
+  {
+    "revision": "93967c7ac0b8a0ecf80e53ebb9e277a4",
+    "url": "/static/media/busdlogo.93967c7a.png"
+  },
+  {
+    "revision": "1478a3b13de302e500d18e4f8f8080e2",
+    "url": "/static/media/bnblogo.1478a3b1.png"
+  },
+  {
+    "revision": "5d1f3b3c4a974b7f21aedc615f4281a5",
+    "url": "/static/media/reilogo.5d1f3b3c.png"
+  },
+  {
+    "revision": "8a2e79d1763a58074b1141f5eeafbd08",
+    "url": "/static/media/nearlogo.8a2e79d1.png"
+  },
+  {
+    "revision": "56c4b08f04d8b7d2dfb02585f4dd04be",
+    "url": "/static/media/kumalogo.56c4b08f.png"
+  },
+  {
+    "revision": "4722fcd65bc4cb5c3d17d831c4a08855",
+    "url": "/static/media/kublogo.4722fcd6.png"
+  },
+  {
+    "revision": "a478c89fb8f1b63b91b795b2b3992915",
+    "url": "/static/media/ethlogo.a478c89f.png"
+  },
+  {
+    "revision": "2f52be5c6a422e9f87b9f6cf10c4920d",
+    "url": "/static/media/aurum.2f52be5c.png"
+  },
+  {
+    "revision": "5c378f02d1168479c37ae442baf49c42",
+    "url": "/static/media/logo.5c378f02.png"
+  },
+  {
+    "revision": "5863c7d5f2a73df0b8340e3fdc26f052",
+    "url": "/index.html"
+  }
+];
